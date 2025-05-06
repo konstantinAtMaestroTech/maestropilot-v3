8978fb955c4dd08dd29111785ce079a999154755
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2025-05-06 22:17:19 +0200

    vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,13 @@ export default defineConfig({
           // Group video assets together
           'video-assets': [
             // List paths to your video files
-            './src/assets/videos/hero-video.webm',
-            './src/assets/videos/mobile-features/identify-component.webm',
-            './src/assets/videos/desktop-features/realtime-tracking.webm',
-            './src/assets/videos/mobile-features/interactive-detail.webm',
-            './src/assets/videos/desktop-features/complex-schedules.webm',
-            './src/assets/videos/mobile-features/attachment-download.webm',
-             './src/assets/videos/desktop-features/generate-reports.webm'
+            './src/assets/videos/Hero-Video.mp4',
+            './src/assets/videos/mobile-features/Identify-Component.mp4',
+            './src/assets/videos/desktop-features/Realtime-Tracking.mp4',
+            './src/assets/videos/mobile-features/Interactive-Detail.mp4',
+            './src/assets/videos/desktop-features/Complex-Schedules.mp4',
+            './src/assets/videos/mobile-features/Attachment-Download.mp4',
+             './src/assets/videos/desktop-features/Generate-Reports.mp4'
             // Add other video paths
           ]
         }
